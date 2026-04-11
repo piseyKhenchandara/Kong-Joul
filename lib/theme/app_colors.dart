eb4179f6e09eabe09eb89429637994134000ea6e
Khen chandarapisey <piseykhenchandara93@gmail.com>
2026-04-11 23:58:12 +0700

    welcome screen

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFF2E7D32);
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color textDark = Color(0xFF1A1A1A);
+  static const Color textMuted = Color(0xFF9E9E9E);
+  static const Color buttonForeground = Color(0xFFFFFFFF);
+}
